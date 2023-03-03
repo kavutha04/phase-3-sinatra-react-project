3992930405dd3a5fbb8d6d03822c595a98ab2621
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-03-03 18:54:12 +0300

    Task migration

---
+++ db/migrate/20230303155252_create_task.rb
@@ -0,0 +1,11 @@
+class CreateTask < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.integer :category_id
+      t.integer :person_id
+      t.timestamp :created_at
+    end
+
+  end
+end
