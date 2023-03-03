c34d9230212eefd80c7050357d1471e522e368e7
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-03-03 23:15:58 +0300

    Task seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,7 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+Task.create([{name: "Take out trash", category_id: 1, person_id: 3}, {name: "Pull weeds", category_id: 2, person_id: 1}, {name: "Buy groceries", category_id: 4, person_id: 2}, {name: "Drop off mail", category_id: 3, person_id: 2}])
+    
+
 puts "✅ Done seeding!"
